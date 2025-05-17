d9636a6feef366213d0702c350cd6e8515b4e54f
geektechdude <34283325+geektechdude@users.noreply.github.com>
2025-05-17 19:18:32 +0100

    cases GET view added

--- flaskCMApp/app/main/views.py
+++ flaskCMApp/app/main/views.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from flask import current_app, render_template, make_response
 from . import main
 
@@ -5,4 +7,22 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     response = make_response(render_template('views/index.html'))
+    return response
+
+
+@main.route('/cases', methods=['GET'])
+def cases():
+    apiURL = "http://192.168.4.230:8000/api/v1/tasks/"
+    getData = requests.get(apiURL)
+
+    if getData.status_code == 200:
+        jsonData = getData.json()
+    else:
+        print(f"Error retrieving data, {getData.status_code}")
+    
+    data = json.dumps(jsonData)
+    cases = json.loads(data)
+
+    response = make_response(render_template('views/cases.html', cases = cases))
+
     return response
